39785eaa01db72a207338acbb88d7e4ea7e4aa2e
12345678Fasai <112545762+12345678Fasai@users.noreply.github.com>
2023-01-23 09:23:45 +0700

    Add files via upload

--- g.7.py
+++ g.7.py
@@ -1,3 +1,5 @@
-number1 = 70
-number2 = 80
-ans = input('do you want to add')
+for i in range (10) : 
+    n1 = int(input('enter number1'))
+    n2 = int(input('enter number2'))
+    result = n1+ n2
+    print (result)
